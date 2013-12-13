2098288651d6aa636e9bc60b98bc3a2d83f86ec8
Nils Esch <r3l04d@gmx.net>
2013-12-13 11:11:20 +0100

    Nur noch quer, dreht net mehr!

--- catchpoint/src/com/example/catchpoint/MainActivity.java
+++ catchpoint/src/com/example/catchpoint/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.catchpoint;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,7 +15,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
 		setContentView(R.layout.activity_main);
 		f = new Field(this);
